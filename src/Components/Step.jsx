db212158851cdd1f5f5178640bc80b71d230198b
Amar Abbas <amarabbas1414@gmail.com>
2024-10-08 00:17:08 +0500

    completed

--- src/Components/Step.jsx
+++ src/Components/Step.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const messages = [
     "Learn React ⚛️",
@@ -7,28 +7,46 @@ const messages = [
   ];
 
 const Step = () => {
-    const step = 1; 
+    const [step,setStep] = useState(1)
+    const [isOpen,setIsOpen] = useState(true)
 
   return (
-    <>
+    <> 
+    <button
+    onClick={()=>{
+        setIsOpen(!isOpen)
+    }}
+    className='close'>&times;</button>
+    { isOpen &&
         <div className='steps'>
             <div className='numbers'>
-                <div className='active'>1</div>
-                <div>2</div>
-                <div>3</div>
+                <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+                <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+                <div className={`${step >= 3 ? "active" : ""}`}>3</div>
             </div>
-            <p className='message'>Step {1}: {messages[step-1]}</p>
+            <p className='message'>Step {step}: {messages[step-1]}</p>
             <div className='buttons'>
-                <button style={{
+                <button 
+                onClick={()=> {
+                  if (step > 1)  setStep(step-1)
+                }}
+                style={{
                     backgroundColor : '#7950f2',
                     color : "#ffffff"
                 }}>Previous</button>
-                <button style={{
+                <button
+                onClick={()=> {
+                    if (step < 3) setStep(step+1)
+                }
+                }
+                style={{
                     backgroundColor : '#7950f2',
                     color : "#ffffff"
                 }}>Next</button>
+                
             </div>
         </div>
+}
     </>
   )
 }
